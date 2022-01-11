fb7b98314ed88a161c66bfec6a18893403fb1051
miya-neee <siriustar.147.8selene@gmail.com>
2022-01-11 11:15:30 +0900

    Change main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from flask import Flask, request, abort
 from linebot import (
     LineBotApi, WebhookHandler
 )
-from linebot.ecceptions import (
+from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
