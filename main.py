250b9a2d8c419ab760d698f20c875c1043230f99
miya-neee <siriustar.147.8selene@gmail.com>
2022-01-11 11:21:59 +0900

    Change main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    FollowEvent, MessageEvent, TextMessage, ImageMessage, ImagesSendMessage, TemplateSendMessage, ButtonTemplate, PostbakTemplateAction, MessageTemplateAction, URITemplateAction
+    FollowEvent, MessageEvent, TextMessage, ImageMessage, ImageSendMessage, TemplateSendMessage, ButtonTemplate, PostbakTemplateAction, MessageTemplateAction, URITemplateAction
 )
 import os
 
