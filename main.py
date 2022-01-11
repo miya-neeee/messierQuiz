2c3c7f470955c85092dea143f23423913c9063a4
miya-neee <siriustar.147.8selene@gmail.com>
2022-01-11 16:25:15 +0900

    change main.py&Procfile

--- main.py
+++ main.py
@@ -17,12 +17,12 @@ import os
 app = Flask(__name__)
 
 ##環境変数からLINE ACCESS TOKENを設定
-LINE_CHANNEL_ACCESS_TOKEN = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
-##LINE_CHANNEL_ACCESS_TOKEN = "GNJfN3CsYSSuqGBDUrwHqDM6vK5TDXy78kVKMhjaOwKn4MtIcIeKyqhoroLO3dmXE6ZqweGqSRTmZPk+ff1EB3AgxhxFZjsE6E8VE3NTaJDEw0Drtuph2TETjmMHjXbNksROAh4euN7vFC9T/+7tAAdB04t89/1O/w1cDnyilFU="
+##LINE_CHANNEL_ACCESS_TOKEN = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
+LINE_CHANNEL_ACCESS_TOKEN = "GNJfN3CsYSSuqGBDUrwHqDM6vK5TDXy78kVKMhjaOwKn4MtIcIeKyqhoroLO3dmXE6ZqweGqSRTmZPk+ff1EB3AgxhxFZjsE6E8VE3NTaJDEw0Drtuph2TETjmMHjXbNksROAh4euN7vFC9T/+7tAAdB04t89/1O/w1cDnyilFU="
 
 ##環境変数からLINE CHANNEL secretを設定
-LINE_CHANNEL_SECRET = os.environ["LINE_CHANNEL_SECRET"]
-##LINE_CHANNEL_SECRET = "d5bbf1a19949be5f16a4d99b3cabad92"
+##LINE_CHANNEL_SECRET = os.environ["LINE_CHANNEL_SECRET"]
+LINE_CHANNEL_SECRET = "d5bbf1a19949be5f16a4d99b3cabad92"
 
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(LINE_CHANNEL_SECRET)
@@ -34,15 +34,18 @@ def callback():
 
     #get request bodyas Text
     body = request.get_data(as_text=True)
-    app.loger.info("Request body: " + body)
+    app.logger.info("Request body: " + body)
     #handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
+        print("Invalid signature, please check your channel access token/channel secret.")
         abort(400)
-        return 'OK'
+
+    return 'OK'
     #MessageEvent
-    @handler.add(MessageEvent, message=TextMessage)
+
+@handler.add(MessageEvent, message=TextMessage)
     def handle_message(event):
         if event.reply_token == "00000000000000000000000000000000":
             return
