ca2836cc885a760c480ee5bbe2010018cade043d
miya-neee <siriustar.147.8selene@gmail.com>
2022-01-11 16:32:45 +0900

    add import_TextSendMessage in main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    FollowEvent, MessageEvent, TextMessage, ImageMessage, ImageSendMessage, TemplateSendMessage, ButtonsTemplate, PostbackTemplateAction, MessageTemplateAction, URITemplateAction
+    FollowEvent, MessageEvent, TextMessage, ImageMessage, ImageSendMessage, TemplateSendMessage, ButtonsTemplate, PostbackTemplateAction, MessageTemplateAction, URITemplateAction, TextSendMessage
 )
 import os
 
