e3149ebe3dc31387199f61d2a158df50b6183d0a
miya-neee <siriustar.147.8selene@gmail.com>
2022-01-11 11:09:19 +0900

    Chage main.py

--- main.py
+++ main.py
@@ -22,8 +22,8 @@ LINE_CHANNEL_ACCEASS_TOKEN = os.environ["LINE_CHANNEL_ACCEASS_TOKEN"]
 LINE_CHANNEL_SECRET = os.environ["LINE_CHANNEL_SECRET"]
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCEASS_TOKEN)
 handler = WebhookHandler(LINE_CHANNEL_SECRET)
-@app.route("/callback",method=['POST'])
-def callback();
+@app.route("/callback",methods=['POST'])
+def callback():
 
     #get X-LineSignature header Value
     signature = request.headers['X-Line-Signature']
